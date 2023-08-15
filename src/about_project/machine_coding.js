0c3badecba3dd22ae5aa9694005be1d9e2d428fe
sourabh <sourabhpande43@gmail.comm>
2023-08-15 16:31:15 +0530

    project-planning

---
+++ src/about_project/machine_coding.js
@@ -0,0 +1,33 @@
+/* 
+🌟♦️machind coding round project 
+
+♦️ Todolist,Tictactoi,Facebook,quiz!!imp Youtube.....
+♦️ More importantly Mangaging you'r time with above project if this is not none of use of any project in you'r mc code interview!! 
+♦️ Practice is mandatory in order to handle such senario which makes you perfect & saves more time !! 
+♦️ Planning helps with pen and paper  
+♦️ for discussing this below thing take 5 min only not too much
+♦️ 5 min for Planning with pen & paper compulsory & discuss with interviewer This thing also (exacalidraw)
+
+@NOTE:
+♦️@ASK_ABOUT: FEATURES
+-Don't Directly jump on VS code Ask for ""Requirements Clarifications"" o.w it's poor Approach
+-Need to ask Question frequently & clear the Requirements!!
+-E.g You ask Question To interviewer What Features we'r going to build Today
+ (Need to discuss this thing compulsory for Mangaging time bcz Can't build large app in 2 hours )
+-For demo take on Youtube Website & ask what Features Need to build this hamburger menu made or not not wheather it is required or not search functionlity need or not like ....
+------------------
+♦️@ASK_ABOUT: Tech Stack
+  -React 
+  -Tailwaind
+  -Redux
+  -react router dom (for routing)
+  -formic (for designing form)
+  -Bundlers (discuss one)
+  -jest (for teasting react library/code)
+  
+ -discuss Tech Stack & ask Question Frequently?
+ -With tech Stack Need to tell Justification why you'r using ??
+ -Let's say as far I know there is UI & Data Layer for UI I preffer to choose "Tailwaind" & for Data Mangaging My personal Choice moves towards "Redux"
+ -Tell about tech stack info Why Tailwaind it's modern technology which makes app faster!
+ -it is easy to write & customizable save us reusability & it is extensible.
+*/
