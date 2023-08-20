6de6fbd516d91d908d290585f02d281ff40a2b04
sourabh <sourabhpande43@gmail.comm>
2023-08-20 11:44:31 +0530

    [Add]-Dyanamic Routing

--- src/about_project/pt.js
+++ src/about_project/pt.js
@@ -19,4 +19,39 @@ Where should i make API Call in 🗃️videoCantainer.js file
 
 @NOTE: Ensure Don't start directly with map Alway start with 0 e.g videos[0] like that 
 
+
+
+
+
+
+-------------------------------SECTION-------------------------------
+@TITLE_OF-DYANAMIC_ROUTING
+@LOCATION: 🗃️WATCHPAGE.JS && 🗃️APP.JS 
+@HINT: BELOW appRouter Notes How It's Work  
+@IDENTIFIER: ^^
+@Task: The Task is befour Header & footer Remain Same While Clicking on Video any video How would you perform this stuff!
+
+
+ -🎯To render this page on via click on video 
+ -🎯First Need to set Router on 🗃️App.js 
+ -🎯createBrowserRouter first then it expect Array of an Object [{}]
+ -🎯then Childern also mandatory 
+ -🎯Fact of Childern is where you defined Or mention "OUTLET" it will shoot out by the term it mean it'll mute/change 
+
+-🎯First thing to note wherever i'll give appRouter my app will render over there or Act According as exact you want!
+  -🎯if my path is "/" slash my Body will be render below <Head/> as i shoot appRouter overThere;
+  -🎯@Childern: it goes wherever My OUTLET exits
+  -🎯Now the path is "/"(slash) what children should be render in my Outlet see in Childern [...] i.e nothing but is <MainContainer/>
+
+  -🎯What should I render here is it App.js or other on click
+  -🎯For that need to create partial route what should on my App(UI)  
+  -🎯As I click on App(Body-Video Cart) should my sidebar & header changes or Remain Same obivious Remain Same
+  -🎯Only {Body} VideoCard Get changes Have you NOTICE(While going to "/watchPage") 
+  -🎯For those Portion Need to work Only i.e mention Body in appRouter when my slash is "/";
+  -🎯Which components you want to change over there need to mention <RouterProvider router={appRouter}/>
+  -🎯Now body has Childern overIt did you see <Sidebar/> and <MainContainer/>
+  -🎯So As per Task Or Route my <MainContainer/> should be Replace with <WatchPage/> either we want both of them At a time"/WatchPage" so in order to do that we Need to mention OUTLET from React router dom 
+  -🎯GIVEN Fact of Childern is wherever you mention Outlet it shoot or change
+
+  Q)How will I configured what'll be the render inside my Outlet!
 */
--- src/components/WatchPage.js
+++ src/components/WatchPage.js
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { closeMenu } from "../utils/appSlice";
+import { useSearchParams } from "react-router-dom";
 
 const WatchPage = () => {
+  let [searchParams, setSearchParams] = useSearchParams();
+  console.log(searchParams.get("v"));
+
+  // Close Menu
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(closeMenu());
+  }, []);
   return (
-    <div>
-      <h1>HELLO,This is Watch WatchPage</h1>
+    <div className='px-5'>
+      <iframe
+        width='1011'
+        height='500'
+        src={"https://www.youtube.com/embed/" + searchParams.get("v")}
+        title='YouTube video player'
+        frameBorder='0'
+        allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
+        allowFullScreen></iframe>
     </div>
   );
 };
@@ -11,10 +29,18 @@ const WatchPage = () => {
 export default WatchPage;
 
 /* 
- @Task: The Task is befour Header & footer Remain Same While Clicking on Video any video How would you perform this stuff!
- -To render this page on via click on video 
- -First Need to set Router on 🗃️App.js 
- -createBrowserRouter first then it expect Array of an Object [{}]
- -then Childern also mandatory 
- -Fact of Childern is where you defined Or mention "OUTLET" it will shoot out by the term it mean it'll mute/change 
+ 0---------------------NEW-------------------0
+
+ -🎯How we CAN build this WatchPage first read URL Id that will helps us to maintaine page 
+ -🎯Why at Beggner Leval my params are empty if if console.log(params)
+ -🎯don't you've Question why because it's has not "/"(slash) something this are search params not normal params How do i find out take reff of below
+
+ REF:->🔗✈️https://reactrouter.com/en/main/hooks/use-search-params
+
+ -🎯We've to use this as a useState 
+ -🎯while log you'll get URLSearchParams this is browser Native URL Search params
+ -🎯with the help of you can querry by searchItem after(?) .get .post like that e.g console.log(searchParams.get()) it'll help you to search what is key over there video key after Question mark ? for that go to 🗃️VideoContainer.js see this Link section 
+ ""<Link to={"/watch?🔻v=" + video.id}>""
+ -🎯with the help of one we get video id in console
+
 */
